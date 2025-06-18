a16f5a454c13c1f6cdc7b3c53d367167c73ad261
Yashraj0808 <bandunikam@outlook.com>
2025-06-18 08:58:35 +0530

    Updated page content and layout

--- firebase.js
+++ firebase.js
@@ -1,20 +1,20 @@
 // firebase.js
-import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore";
-import { getStorage } from "firebase/storage";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.22.1/firebase-app.js";
+import { getFirestore, collection, addDoc, getDocs, doc, setDoc } from "https://www.gstatic.com/firebasejs/9.22.1/firebase-firestore.js";
 
+// 🔒 Your Firebase config from the console
 const firebaseConfig = {
-  apiKey: "AIzaSyA6ESBH4HR7BWQ8814iuEAGORR1rWxd9TA",
-  authDomain: "ashish-general-store.firebaseapp.com",
-  projectId: "ashish-general-store",
-  storageBucket: "ashish-general-store.appspot.com",
-  messagingSenderId: "483265427829",
-  appId: "1:483265427829:web:6b61fb1b3f661b8dffbb3c",
-  measurementId: "G-1FW7VCCXXM"
+  apiKey: "YOUR_API_KEY",
+  authDomain: "YOUR_PROJECT.firebaseapp.com",
+  projectId: "YOUR_PROJECT_ID",
+  storageBucket: "YOUR_PROJECT.appspot.com",
+  messagingSenderId: "YOUR_SENDER_ID",
+  appId: "YOUR_APP_ID"
 };
 
 const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
 
-// Export Firestore and Storage
-export const db = getFirestore(app);
-export const storage = getStorage(app);
+// Make `db` global so products.html can use it
+window.db = db;
+window.firestoreFns = { addDoc, collection, getDocs, doc, setDoc };
